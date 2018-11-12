26ef8e879f093ba472aa9caa33eab03ff01e4758
Pablo Sichert <mail@pablosichert.com>
2018-11-13 00:53:27 +0100

    Show timestamps in bash history

--- .bashrc.d/default/history.sh
+++ .bashrc.d/default/history.sh
@@ -8,3 +8,6 @@ shopt -s histappend
 # For setting history length see HISTSIZE and HISTFILESIZE in bash(1)
 HISTSIZE= # The sky is the limit
 HISTFILESIZE= # The sky is the limit
+
+# Show timestamps in history
+HISTTIMEFORMAT="%F %T  "
