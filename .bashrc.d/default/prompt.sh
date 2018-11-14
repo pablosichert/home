204cd4e9b86d8230b4f259d927b723e9a2e47444
Pablo Sichert <mail@pablosichert.com>
2018-11-14 20:29:08 +0100

    Change prompt color for ssh connections

--- .bashrc.d/default/prompt.sh
+++ .bashrc.d/default/prompt.sh
@@ -1,13 +1,17 @@
 DIM="\e[2m"
 BLUE="\e[38;2;0;160;255m"
+ORANGE="\e[38;2;255;160;0m"
 END="\e[0m"
 
+PROMPT_COLOR="$BLUE"
+
 if [ -n "$SSH_CONNECTION" ]; then
+    PROMPT_COLOR="$ORANGE"
     PROMPT_PREFIX="\h "
 fi
 
 function prompt {
-    PS1="$PROMPT_PREFIX$HOST\[$DIM\]\W\[$END\] \[$BLUE\]$\[$END\] "
+    PS1="$PROMPT_PREFIX$HOST\[$DIM\]\W\[$END\] \[$PROMPT_COLOR\]$\[$END\] "
 }
 
 PROMPT_COMMAND=prompt
